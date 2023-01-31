c12e56400feb8e7070e5a926afd98f6c6fff52e0
AxelManns <94969028+AxelManns@users.noreply.github.com>
2023-01-31 11:48:47 +0100

    Log in funkar nu

--- app.rb
+++ app.rb
@@ -6,14 +6,54 @@ require 'bcrypt'
 
 enable :sessions
 
+before do
+    p request.request_method
+end
+
+def change_routes(current_route)
+    session[:last_route_visited] = session[:current_route]
+    session[:current_route] = current_route
+end
+
+def get_dataBase()
+    db = SQLite3::Database.new("db/Database.db")
+    db.results_as_hash = true
+    return db
+end
+
 get('/') do
+    change_routes('/')
     slim(:main)
+end
+
+post('/log_in') do
+    username = params["username"]
+    password = params["password"]
+    db = get_dataBase()
+    password_from_db = db.execute("SELECT password_digest FROM Users WHERE username = ?", username).first
+    p password_from_db
+    if password_from_db == nil
+        session[:raise_error] = true
+        session[:error_message] = "Username does not exist"
+    elsif BCrypt::Password.new(password_from_db["password_digest"]) == password
+        session[:user] = db.execute("SELECT * FROM Users WHERE username = ?", username).first
+    else
+        session[:raise_error] = true
+        session[:error_message] = "Password is incorrect"
+    end
+    redirect("#{session[:last_route_visited]}")
+end
+
+post('/log_out') do 
+    session[:user] = nil
+    redirect("#{session[:last_route_visited]}")
 end
 
 get('/users/new') do
     if session[:last_route_visited] == nil
         session[:last_route_visited] = "/users/new"
     end
+    change_routes(request.path_info)
     slim(:register)
 end
 
@@ -21,8 +61,7 @@ post('/users') do
     new_username = params["username"]
     password = params["password"]   
     password_conf = params["password_confirmation"]
-    db = SQLite3::Database.new("db/Database.db")
-    db.results_as_hash = true
+    db = get_dataBase()
     username_arr = db.execute("SELECT username FROM Users")
     i = 0
     username_exists = false
--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,8 @@ const log_in_form = document.getElementById("log_in_form")
 
 function toggleLogInForm() {
     log_in_form.classList.toggle("log_in_active")
+    console.log(log_in_form)
 }
 
+console.log("this happens")
 log_in_button.addEventListener("click", toggleLogInForm)
